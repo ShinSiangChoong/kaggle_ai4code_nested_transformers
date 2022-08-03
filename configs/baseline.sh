4fc7fa445f6edd48621924407eb59d050091f533
erniechiew <ernchiew@gmail.com>
2022-08-03 18:03:23 +0000

    Update config

--- configs/baseline.sh
+++ configs/baseline.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/sh
 
 python src/training/train.py \
---max_n_cells 126 \
+--model_name_or_path 'microsoft/codebert-base' \
+--tokenizer_name_or_path './outputs/tokenizer_codebert_ext_data' \
+--pretrained_mlm_path './outputs/model-5.bin' \
+--max_n_cells 254 \
 --max_len 64 \
---batch_size 4 \
---accumulation_steps 4 \
+--batch_size 2 \
+--accumulation_steps 8 \
 --epochs 30 \
 --n_workers 1 \
 --wandb_mode online
